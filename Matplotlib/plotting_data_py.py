ba4034828b9f88ce0e6f9a9d8a40678ba90d93a1
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2024-10-19 16:46:28 +0700

    Add files via upload

---
+++ Matplotlib/plotting_data_py.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+view=[202,121,312,514,212,123,454,546]
+days=range(1,9)
+print(len(view))
+
+plt.figure(figsize=(10,4))
+plt.plot(days,view,label='Random Number',color='red')
+plt.xlabel('Hari')
+plt.ylabel('Angka')
+plt.legend()
+plt.show()
+
